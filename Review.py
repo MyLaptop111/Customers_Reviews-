d95e5a55c50b383df8bba2efbd1872acc53c5b63
Mohab Mohamed <Mylaptop1119@gmail.com>
2026-01-21 21:08:40 +0200

    Add files via upload

---
+++ Review.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Customer Review", layout="centered")
+
+st.title("📝 Customer Review Form")
+
+name = st.text_input("Your Name")
+review = st.text_area("Write your review")
+
+submit = st.button("Submit")
+
+if submit:
+    if name == "" or review == "":
+        st.error("Please fill all fields")
+    else:
+        payload = {
+            "name": name,
+            "review": review
+        }
+
+        # رابط Webhook من n8n
+        webhook_url = "http://localhost:5678/webhook-test/2ebf9bdf-cb4f-4790-90b1-9a613c1cb1f4"
+
+        response = requests.post(webhook_url, json=payload)
+
+        if response.status_code == 200:
+            st.success("✅ Review submitted successfully")
+        else:
+            st.error("❌ Something went wrong")
